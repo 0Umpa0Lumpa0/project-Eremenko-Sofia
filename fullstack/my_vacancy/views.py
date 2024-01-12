747282157d25e3b4d119ccea314ec95164f4998c
0Umpa0Lumpa0 <Eremenko.Sofia@urfu.me>
2024-01-12 12:15:30 +0500

    add crud form main page

--- fullstack/my_vacancy/views.py
+++ fullstack/my_vacancy/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import View
 from my_vacancy.models import *
 
 
-class IndexView(ListView):
+class BasePageView(View):
+    template_name = None
+    model = None
+    context_name = None
+
+    def get_queryset(self):
+        return self.model.objects.all()
+
+    def get_context_data(self, **kwargs):
+        return {'navigation': Navigations.objects.all(), 'context': self.get_queryset(), **kwargs}
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.get_context_data())
+
+
+class IndexView(BasePageView):
     model = MainInfo
     template_name = 'index.html'
 
 
-class Importance(ListView):
+class Importance(BasePageView):
     model = JobDemand
     template_name = 'importance.html'
 
 
-class Geography(ListView):
+class Geography(BasePageView):
     model = CityStatistics
     template_name = 'geography.html'
 
 
-class SkillSet(ListView):
+class SkillSet(BasePageView):
     model = SkillSet
     template_name = 'skillset.html'
 
 
-class LastVacancy(ListView):
+class LastVacancy(BasePageView):
     model = RecentVacancies
     template_name = 'last_vacancy.html'
